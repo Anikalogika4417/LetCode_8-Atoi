4ddd766c61ce292411d4d6d2b270d28396d6f3c7
Nuta <111691010+Anikalogika4417@users.noreply.github.com>
2023-11-09 20:55:15 +0200

    Add files via upload

---
+++ LetCode8/LetCode8.cpp
@@ -0,0 +1,18 @@
+// LetCode8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    int myAtoi(string s) {
+
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
