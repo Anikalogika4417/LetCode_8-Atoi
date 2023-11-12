f777b1149a60d04e673103b8ab2bdf945cf897f6
Anikalogika4417 <anikalogika4417@gmail.com>
2023-11-12 21:32:33 +0200

    firsrt_view

--- LetCode8/LetCode8.cpp
+++ LetCode8/LetCode8.cpp
@@ -3,11 +3,30 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+
+using namespace std;
 
 class Solution {
 public:
     int myAtoi(string s) {
+        map<char, int> my_map{};
+        my_map['0'] = 0;
+        my_map['1'] = 1;
+        my_map['2'] = 2;
+        my_map['3'] = 3;
+        my_map['4'] = 4;
+        my_map['5'] = 5;
+        my_map['6'] = 6;
+        my_map['7'] = 7;
+        my_map['8'] = 8;
+        my_map['9'] = 9;
 
+        for (int i = 0; i < s.size(); i++) {
+            if (my_map.find(s[i]) != my_map.end()) {
+
+            }
+        }
     }
 };
 
